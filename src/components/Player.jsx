266feb422bd25e86b80609fba322a0f9fd7b7923
Emanuel Peire <emapeire@outlook.com>
2023-12-29 16:03:45 -0300

    Add Vercel adapter and update Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const Pause = ({ className }) => (
   <svg
@@ -59,6 +59,23 @@ export const Volume = () => (
 
 export function Player() {
   const [isPlaying, setIsPlaying] = useState(false)
+  const [currentSong, setCurrentSong] = useState(null)
+  const audioRef = useRef()
+
+  useEffect(() => {
+    audioRef.current.src = `/music/1/01.mp3`
+  }, [])
+
+  const handleClick = () => {
+    if (isPlaying) {
+      audioRef.current.pause()
+    } else {
+      audioRef.current.play()
+      audioRef.current.volume = 0.5
+    }
+
+    setIsPlaying(!isPlaying)
+  }
 
   return (
     <div className='flex flex-row justify-between w-full px-4 z-50'>
@@ -66,16 +83,14 @@ export function Player() {
 
       <div className='grid place-content-center gap-4 flex-1'>
         <div className='flex justify-center'>
-          <button
-            className='bg-white rounded-full p-2'
-            onClick={() => setIsPlaying(!isPlaying)}
-          >
+          <button className='bg-white rounded-full p-2' onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
+          <audio ref={audioRef} />
         </div>
       </div>
 
-      <div>Volume Controls...</div>
+      <div className='grid place-content-center'>Volume Controls...</div>
     </div>
   )
 }
