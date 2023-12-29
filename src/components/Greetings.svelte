3ce65f805bed3537208fad6eb6d5d91e7ddd5c75
Emanuel Peire <emapeire@outlook.com>
2023-12-29 14:09:47 -0300

    Add Greetings component to index page

---
+++ src/components/Greetings.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greetings = 'Greetings'
+
+  if (currentHour < 12) {
+    greetings = 'Good morning'
+  } else if (currentHour < 18) {
+    greetings = 'Good afternoon'
+  } else {
+    greetings = 'Good evening'
+  }
+</script>
+
+<h1 class="text-4xl font-bold">{greetings}</h1>
