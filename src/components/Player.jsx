cf8f57479e6471def63ac0aefa6efe9b7f08c5a9
Emanuel Peire <emapeire@outlook.com>
2023-12-29 14:22:57 -0300

    Add Player component to footer in Layout.astro

---
+++ src/components/Player.jsx
@@ -0,0 +1,11 @@
+export function Player() {
+  return (
+    <div className='flex flex-row justify-between w-full px-4 z-50'>
+      <div>Current Song...</div>
+
+      <div>Player Controls...</div>
+
+      <div>Volume Controls...</div>
+    </div>
+  )
+}
