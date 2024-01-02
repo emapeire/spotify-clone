133975ef460a2fc2f4080f65397742dd0e3b96c2
Emanuel Peire <emapeire@outlook.com>
2024-01-02 15:11:59 -0300

    Update ESLint configuration and fix playlist page layout

--- src/pages/api/get-info-playlist.json.js
+++ src/pages/api/get-info-playlist.json.js
@@ -1,14 +1,14 @@
 import { allPlaylists, allSongs } from '@/lib/data'
 
-export async function GET({ params, request }) {
-  const { url } = request
-  const urlObject = new URL(url)
-  const id = urlObject.searchParams.get('id')
+export async function GET({ _params, request }) {
+	const { url } = request
+	const urlObject = new URL(url)
+	const id = urlObject.searchParams.get('id')
 
-  const playlist = allPlaylists.find((playlist) => playlist.id === id)
-  const songs = allSongs.filter((song) => song.albumId === playlist?.albumId)
+	const playlist = allPlaylists.find((playlist) => playlist.id === id)
+	const songs = allSongs.filter((song) => song.albumId === playlist?.albumId)
 
-  return new Response(JSON.stringify({ playlist, songs }), {
-    headers: { 'Content-Type': 'application/json' }
-  })
+	return new Response(JSON.stringify({ playlist, songs }), {
+		headers: { 'Content-Type': 'application/json' }
+	})
 }
--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -1,10 +1,10 @@
 import { create } from 'zustand'
 
 export const usePlayerStore = create((set) => ({
-  isPlaying: false,
-  setIsPlaying: (isPlaying) => set({ isPlaying }),
-  currentSong: { playlist: null, song: null, songs: [] },
-  setCurrentSong: (currentSong) => set({ currentSong }),
-  volume: 1,
-  setVolume: (volume) => set({ volume })
+	isPlaying: false,
+	setIsPlaying: (isPlaying) => set({ isPlaying }),
+	currentSong: { playlist: null, song: null, songs: [] },
+	setCurrentSong: (currentSong) => set({ currentSong }),
+	volume: 1,
+	setVolume: (volume) => set({ volume })
 }))
